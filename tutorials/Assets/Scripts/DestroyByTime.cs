13fe4fc91a94c4754cebfc37dfde18aad72a1610
Ivan Milev <ivanmilevTUES@gmail.com>
2016-02-03 00:19:41 +0200

    UnityGame1
    SpaceGame You can shoot some stuff ;)
    Added the asteroids.
    Need to add ScoreBoard
    Need to add Finisher
    Need to add the Enemy Spaceships.

---
+++ tutorials/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float liveTime;
+
+    void Start()
+    {
+        Destroy(gameObject,liveTime);
+    }
+}
